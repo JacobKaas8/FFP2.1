ce92687aaec4c3469051c13ce057c19a5f7b4433
Jonatan Emil Rytter <jor001@edu.zealand.dk>
2024-03-05 11:25:53 +0100

    Search bar Kategori progress

--- global.php
+++ global.php
@@ -1,8 +1,15 @@
 <nav class="navbar bg-body-tertiary">
-    <form class="container-fluid">
-        <div class="input-group">
-            <span class="input-group-text" id="basic-addon1">@</span>
-            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+    <div class="container">
+    <form>
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <div class="input-group">
+                    <span class="input-group-text" id="basic-addon1">@</span>
+                    <input type="text" class="form-control" placeholder="Username" aria-label="Username"
+                           aria-describedby="basic-addon1">
+                </div>
+            </div>
         </div>
     </form>
+    </div>
 </nav>
