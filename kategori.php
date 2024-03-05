20a6993d5101907c53321ed7888522c62abbe355
Jonatan Emil Rytter <jor001@edu.zealand.dk>
2024-03-05 11:11:40 +0100

    Search bar Kategori progress

--- kategori.php
+++ kategori.php
@@ -1,1 +1,48 @@
 <?php
+require "settings/init.php";
+?>
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Kategori Liste</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+<?php include("global.php");?>
+<div class="row g-5 justify-content-center text-center mt-5">
+    <?php
+    $categories = $db->sql("SELECT * FROM categories");
+    foreach($categories as $category) {
+        //echo $category->categoryColor;
+        ?>
+        <div class="col-4 bg-success p-3">
+            <img src="<?php echo $category->categoryPicture; ?>" class="opacity-75">
+                    <?php
+                    echo $category->categoryName;
+                    ?>
+
+                    <?php
+                    // Indsæt andet felt fra database
+                    ?>
+                    <?php
+                    // Indsæt andet felt fra database
+                    ?>
+        </div>
+        <?php
+    }
+    ?>
+</div>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
