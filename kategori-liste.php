9bf670739b2e36eb418231151ccbecb1454984b5
Sebastian <sec001@edu.zealand.dk>
2024-03-06 11:17:17 +0100

    kategori-liste.php html kode lavet

--- kategori-liste.php
+++ kategori-liste.php
@@ -26,33 +26,70 @@ require "settings/init.php";
 <div class="container-fluid">
     <div class="row">
         <div class="col-2">
-            <div class="d-flex justify-content-center">
-                <div class="d-flex position-relative justify-content-center bookmark bg-catCol1 w-75">
-                    <a href="#" class="stretched-link"></a>
-                    <div class="position-absolute bottom-0 pb-4">
-                        <h3 class="text-light fw-semibold">Playstation</h3>
+            <div class="sticky-top">
+                <div class="d-flex justify-content-center">
+                    <div class="d-flex position-relative justify-content-center bookmark bg-catCol1 w-75">
+                        <a href="#" class="stretched-link"></a>
+                        <div class="position-absolute bottom-0 pb-4">
+                            <h3 class="text-light fw-semibold">Playstation</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center mt-4">
+                    <div class="d-flex flex-column gap-3">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault1">
+                            <label class="form-check-label" for="flexCheckDefault1">
+                                Action
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault2">
+                            <label class="form-check-label" for="flexCheckDefault2">
+                                Adventure
+                            </label>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="d-flex justify-content-center mt-4">
-                <div class="d-flex flex-column gap-3">
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault1">
-                        <label class="form-check-label" for="flexCheckDefault1">
-                            Action
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault2">
-                        <label class="form-check-label" for="flexCheckDefault2">
-                            Adventure
-                        </label>
-                    </div>
-                </div>
-            </div>
+
         </div>
         <div class="col-8">
             <?php include("global.php"); ?>
+            <div class="row mt-5">
+                <div class="col-4 p-3">
+                    <div class="produkt-box position-relative">
+                        <a href="#" class="stretched-link"><img src="img/kirbys-dream-land-game-boy.webp" alt="Kirby Dream Land"></a>
+                        <h2 class="pt-2">Kirby Dream Land</h2>
+                        <span>Action, Adventure,</span>
+                        <p class="fs-1 fw-semibold pt-2">200,- DKK</p>
+                    </div>
+                </div>
+                <div class="col-4 p-3">
+                    <div class="produkt-box position-relative">
+                        <a href="#" class="stretched-link"><img src="img/kirbys-dream-land-game-boy.webp" alt="Kirby Dream Land"></a>
+                        <h2 class="pt-2">Kirby Dream Land</h2>
+                        <span>Action, Adventure,</span>
+                        <p class="fs-1 fw-semibold pt-2">200,- DKK</p>
+                    </div>
+                </div>
+                <div class="col-4 p-3">
+                    <div class="produkt-box position-relative">
+                        <a href="#" class="stretched-link"><img src="img/kirbys-dream-land-game-boy.webp" alt="Kirby Dream Land"></a>
+                        <h2 class="pt-2">Kirby Dream Land</h2>
+                        <span>Action, Adventure,</span>
+                        <p class="fs-1 fw-semibold pt-2">200,- DKK</p>
+                    </div>
+                </div>
+                <div class="col-4 p-3">
+                    <div class="produkt-box position-relative">
+                        <a href="#" class="stretched-link"><img src="img/kirbys-dream-land-game-boy.webp" alt="Kirby Dream Land"></a>
+                        <h2 class="pt-2">Kirby Dream Land</h2>
+                        <span>Action, Adventure,</span>
+                        <p class="fs-1 fw-semibold pt-2">200,- DKK</p>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="col-2 mt-5">
             <?php include("menu.php");?>
