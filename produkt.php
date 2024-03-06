04152f99864044191466ec35da922271d108294f
Sebastian <sec001@edu.zealand.dk>
2024-03-06 13:48:45 +0100

    produkt.php klar til java og php

--- produkt.php
+++ produkt.php
@@ -18,7 +18,8 @@ require "settings/init.php";
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet">
 </head>
 
 <body>
@@ -60,18 +61,94 @@ require "settings/init.php";
                     <a href="#"><img src="img/kirbys-dream-land-game-boy.webp" alt="Kirby Dream Land" class="w-100"></a>
                 </div>
                 <div class="col-8 p-3">
-                    <h2 class="pt-2">Kirby Dream Land</h2>
-                    <span>Action, Adventure,</span>
-                    <p class="pt-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                    <a href="#" class="link-primary">Læs mere</a>
+                    <h2 class="pt-2 fw-semibold">Kirby Dream Land</h2>
+                    <span><a href="#">Action</a>&nbsp;&nbsp;<a href="#">Adventure</a></span>
+                    <p class="pt-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                        Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
+                        took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy
+                        text of the printing and typesetting industry.</p>
+                    <a href="#beskrivelse" class="link-primary">Læs mere</a>
                     <p class="fs-1 fw-semibold pt-2">200,- DKK</p>
-                    <a href="#"><button class="btn border border-2 border-primary link-primary fw-semibold px-5 py-2 me-3">Læg i kurv</button></a>
-                    <a href="#"><button class="btn border border-2 border-primary link-primary fw-semibold px-5 py-2">Prøv spillet</button></a>
+                    <a href="#">
+                        <button class="btn border border-2 border-primary link-primary fw-semibold px-5 py-2 me-3">Læg i
+                            kurv
+                        </button>
+                    </a>
+                    <a href="#">
+                        <button class="btn border border-2 border-primary link-primary fw-semibold px-5 py-2">Prøv
+                            spillet
+                        </button>
+                    </a>
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col-4">
+                    <h2 class="pt-2 fw-semibold">Information</h2>
+                    <table>
+                        <tr>
+                            <td class="pt-2">Udgivelsesår:</td>
+                            <td class="pt-2 ps-3">1999</td>
+                        </tr>
+                        <tr>
+                            <td class="pt-2">Udgiver:</td>
+                            <td class="pt-2 ps-3">Navn</td>
+                        </tr>
+                        <tr>
+                            <td class="pt-2">Udvikler:</td>
+                            <td class="pt-2 ps-3">Navn</td>
+                        </tr>
+                        <tr>
+                            <td class="pt-2">Stand:</td>
+                            <td class="pt-2 ps-3">Brugt</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="col-8">
+                    <h2 class="pt-2 fw-semibold" id="beskrivelse">Beskrivelse</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                        of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the
+                        printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever
+                        since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
+                        specimen book. It has survived not only five centuries, but also the leap into electronic
+                        typesetting.</p>
+                </div>
+            </div>
+            <div class="row mt-4 mb-4">
+                <div class="col-12">
+                    <h2 class="pt-2 fw-semibold">Relaterede produkter</h2>
+                </div>
+                <div class="col-4 p-3">
+                    <div class="produkt-box position-relative">
+                        <a href="#" class="stretched-link"><img src="img/kirbys-dream-land-game-boy.webp"
+                                                                alt="Kirby Dream Land"></a>
+                        <h2 class="pt-2">Kirby Dream Land</h2>
+                        <span>Action, Adventure,</span>
+                        <p class="fs-1 fw-semibold pt-2">200,- DKK</p>
+                    </div>
+                </div>
+                <div class="col-4 p-3">
+                    <div class="produkt-box position-relative">
+                        <a href="#" class="stretched-link"><img src="img/kirbys-dream-land-game-boy.webp"
+                                                                alt="Kirby Dream Land"></a>
+                        <h2 class="pt-2">Kirby Dream Land</h2>
+                        <span>Action, Adventure,</span>
+                        <p class="fs-1 fw-semibold pt-2">200,- DKK</p>
+                    </div>
+                </div>
+                <div class="col-4 p-3">
+                    <div class="produkt-box position-relative">
+                        <a href="#" class="stretched-link"><img src="img/kirbys-dream-land-game-boy.webp"
+                                                                alt="Kirby Dream Land"></a>
+                        <h2 class="pt-2">Kirby Dream Land</h2>
+                        <span>Action, Adventure,</span>
+                        <p class="fs-1 fw-semibold pt-2">200,- DKK</p>
+                    </div>
                 </div>
             </div>
         </div>
         <div class="col-2 mt-5">
-            <?php include("menu.php");?>
+            <?php include("menu.php"); ?>
         </div>
     </div>
 </div>
