7d67dbc671ff130590a003530348134bafd45196
Jonatan Emil Rytter <jor001@edu.zealand.dk>
2024-03-10 20:56:23 +0100

    Kurv side rigtig uden productId

--- settings/init.php
+++ settings/init.php
@@ -8,10 +8,10 @@ error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED & ~E_STRICT);
 define("CONFIG_LIVE", "0"); // 0: Test enviroment || 1: Live enviroment
 
 if(CONFIG_LIVE == 0){
-    $DB_SERVER = "localhost";
-    $DB_NAME = "interdisplay";
-    $DB_USER = "root";
-    $DB_PASS = "";
+    $DB_SERVER = "mysql6.unoeuro.com"; //localhost
+    $DB_NAME = "jerytter_dk_db_interdisplay"; //interdisplay
+    $DB_USER = "jerytter_dk"; // root
+    $DB_PASS = "B4GFzek5wb2aREp6yhAn"; // ikke noget her!
 }else{
     $DB_SERVER = "";
     $DB_NAME = "";
