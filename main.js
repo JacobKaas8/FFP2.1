a8dabb857dba08a911a6cdf19cf0feb83b24f671
jacob <jan002@edu.zealand.dk>
2024-03-10 21:31:00 +0100

    baggrund animation igang, main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+let headerBackgrounds = document.querySelectorAll(".background");
+
+let imageindex = 0;
+
+function changeBackground() {
+    // Remove .showing class from current background
+    headerBackgrounds[imageindex].classList.remove("showing");
+
+    // Increment the image index by one
+    imageindex++;
+
+    // If the image index is more than there are elements, set it to 0
+    if (imageindex>= headerBackgrounds.length) {
+        imageindex = 0
+    }
+
+    // Add the .showing class to the next background
+    headerBackgrounds[imageindex].classList.add("showing")
+}
+
+setInterval(changeBackground, 3000)
